c014e59615645103aff43c49264205bff0709a57
Connor <conkelley7@gmail.com>
2025-06-02 09:08:09 -0500

    initial commit

---
+++ src/main/java/com/kelley/medicationassistant/MedicationassistantApplication.java
@@ -0,0 +1,13 @@
+package com.kelley.medicationassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicationassistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelley/medicationassistant/MedicationassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelley.medicationassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationassistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
